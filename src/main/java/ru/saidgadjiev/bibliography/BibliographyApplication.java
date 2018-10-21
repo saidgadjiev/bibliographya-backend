540d8953e5ac84bc729005bb2dd828cb48ce4017
GadzhievSA <said.gadzhiev@id-mt.ru>
2018-10-21 23:34:09 +0300

    Initial commit

---
+++ src/main/java/ru/saidgadjiev/bibliography/BibliographyApplication.java
@@ -0,0 +1,12 @@
+package ru.saidgadjiev.bibliography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliographyApplication.class, args);
+	}
+}
