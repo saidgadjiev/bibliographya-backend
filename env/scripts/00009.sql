913621877c974a7ca7fada9db35ae4defc76680d
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-02-01 18:52:03 +0300

    Каскадное удаление связанных с биографией сущностей если удалится биография

--- env/scripts/00009.sql
+++ env/scripts/00009.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS biography_report (
   reported_at TIMESTAMP DEFAULT NOW(),
   reporter_id INTEGER NOT NULL REFERENCES "user"(id),
   consider_id INTEGER REFERENCES "user"(id),
-  biography_id INTEGER NOT NULL REFERENCES biography(id),
+  biography_id INTEGER NOT NULL REFERENCES biography(id) ON DELETE CASCADE,
   status INTEGER NOT NULL DEFAULT 0,
   reason INTEGER NOT NULL,
   reason_text TEXT
