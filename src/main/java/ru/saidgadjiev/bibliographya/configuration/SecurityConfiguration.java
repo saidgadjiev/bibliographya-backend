7e3b8e6424bbe29b30983cb407ebe6f3dd58ef7f
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-04-05 07:14:04 +0300

    cors для set-cookie

--- src/main/java/ru/saidgadjiev/bibliographya/configuration/SecurityConfiguration.java
+++ src/main/java/ru/saidgadjiev/bibliographya/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -173,7 +174,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
         configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setExposedHeaders(Collections.singletonList(jwtProperties.tokenName()));
+        configuration.setExposedHeaders(Arrays.asList(jwtProperties.tokenName(), HttpHeaders.SET_COOKIE));
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
