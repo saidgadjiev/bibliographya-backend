d57d979f79b648c06c1ff77c80505f34ae9938e9
GadzhievSA <s.gadzhiev@id-mt.ru>
2019-04-13 09:10:48 +0300

    Фикс ресайз картинки

--- src/main/java/ru/saidgadjiev/bibliographya/service/impl/MagickService.java
+++ src/main/java/ru/saidgadjiev/bibliographya/service/impl/MagickService.java
@@ -31,13 +31,12 @@ public class MagickService {
 
         operation.addImage(appProperties.getResources() + File.separator + AppProperties.SHARE_PATH);
         operation.addRawArgs("-pointsize", "70");
-        operation.addRawArgs("-font", "Roboto-Black");
-        operation.addRawArgs("-gravity", "Center");
-        operation.addRawArgs("-annotate", "0");
-
         if (StringUtils.isNotBlank(magickSize)) {
             operation.addRawArgs("-resize", magickSize);
         }
+        operation.addRawArgs("-font", "Roboto-Black");
+        operation.addRawArgs("-gravity", "Center");
+        operation.addRawArgs("-annotate", "0");
         operation.addRawArgs(magickText);
 
         Path resultPath = Paths.get(storageProperties.getRoot()).resolve(storageProperties.getShareRoot());
